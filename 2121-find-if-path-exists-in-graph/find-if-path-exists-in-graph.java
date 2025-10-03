99403966c0768da8475c02f30974219a1c79888f
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-03 15:35:52 +0530

    Time: 165 ms (30.32%) | Memory: 192.9 MB (14.06%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -9,7 +9,6 @@ class Solution {
             lt.get(i[0]).add(i[1]);
             lt.get(i[1]).add(i[0]);
         }
-        // for(List<Integer> i:lt)System.out.println(lt);
         track[s]=true;
         return dfs(s,d);
     }
