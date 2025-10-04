d9a10efbbe04d8293108c75d475378417321b3dc
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-04 13:58:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (34.17%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int[][] nums;
+    public static boolean[][] track;
+    public static int val;
+    public int[][] floodFill(int[][] nums, int row, int col, int k) {
+        this.nums = nums;
+        track = new boolean[nums.length][nums[0].length];
+        val = nums[row][col];
+        dfs(row,col,k);
+        return nums;
+    }
+    public static void dfs(int row, int col, int k){
+        if(row<0 || col<0 || row==nums.length || col==nums[0].length || track[row][col] || nums[row][col]!=val) return;
+        if(!track[row][col]){
+            track[row][col] = true;
+            nums[row][col] = k;
+            dfs(row+1,col,k);
+            dfs(row,col+1,k);
+            dfs(row-1,col,k);
+            dfs(row,col-1,k);
+        }
+    }
+}
