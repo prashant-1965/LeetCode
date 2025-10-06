4e2c2ebf610f4503dd3b6ab686b33702744a5100
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-06 19:00:04 +0530

    Time: 7 ms (16.09%) | Memory: 52.8 MB (7.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,6 +2,7 @@ class Solution {
     public static boolean[][] track;
     public static Queue<int[]> q;
     public static char[][] nums;
+    public static int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
     public int numIslands(char[][] nums) {
         int cnt=0;
         this.nums=nums;
@@ -13,30 +14,23 @@ class Solution {
                     this.q = new LinkedList<>();
                     q.add(new int[]{i,j});
                     track[i][j]=true;
-                    bfs(i,j);
+                    bfs();
                 }
             }
         }
         return cnt;
     }
-    public static void bfs(int row, int col){
-        while(!q.isEmpty()){
-            int[] temp = q.poll();
-            if(temp[0]-1>=0 && !track[temp[0]-1][temp[1]] && nums[temp[0]-1][temp[1]]=='1'){
-                track[temp[0]-1][temp[1]]=true;
-                q.add(new int[]{temp[0]-1,temp[1]});
-            }
-            if(temp[0]+1<nums.length && !track[temp[0]+1][temp[1]] && nums[temp[0]+1][temp[1]]=='1'){
-                track[temp[0]+1][temp[1]] = true;
-                q.add(new int[]{temp[0]+1,temp[1]});
-            }
-            if(temp[1]-1>=0 && !track[temp[0]][temp[1]-1] && nums[temp[0]][temp[1]-1]=='1'){
-                track[temp[0]][temp[1]-1]=true;
-                q.add(new int[]{temp[0],temp[1]-1});
-            }
-            if(temp[1]+1<nums[0].length && !track[temp[0]][temp[1]+1] && nums[temp[0]][temp[1]+1]=='1'){
-                track[temp[0]][temp[1]+1]=true;
-                q.add(new int[]{temp[0],temp[1]+1});
+    public static void bfs(){
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int[] d : dirs) {
+                int r = cur[0] + d[0];
+                int c = cur[1] + d[1];
+                if (r >= 0 && c >= 0 && r < nums.length && c < nums[0].length &&
+                    nums[r][c] == '1' && !track[r][c]) {
+                    track[r][c] = true;
+                    q.add(new int[]{r, c});
+                }
             }
         }
     }
