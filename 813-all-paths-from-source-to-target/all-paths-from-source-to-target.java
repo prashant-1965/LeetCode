6ee89e1a68e105c6f4cb9cc90a3c3751e7b95762
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-13 23:32:59 +0530

    Time: 2 ms (68.86%) | Memory: 46.3 MB (63.31%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,25 +1,21 @@
 class Solution {
     public static List<List<Integer>> ans;
     public List<List<Integer>> allPathsSourceTarget(int[][] nums) {
-        boolean[] track = new boolean[nums.length];
         ans = new ArrayList<>();
-        dfs(0,new ArrayList<>(),nums,track);
+        dfs(0,new ArrayList<>(),nums);
         return ans;
     }
-    public static void dfs(int n, List<Integer> lt, int[][] nums, boolean[] track){
+    public static void dfs(int n, List<Integer> lt, int[][] nums){
         if(n==nums.length-1 || (nums[n].length==0 && n==nums.length-1)){
             lt.add(n);
             ans.add(new ArrayList<>(lt));
             lt.remove(lt.size()-1);
             return;
         }
-        if(track[n]) return;
-        track[n]=true;
         lt.add(n);
         for(int i=0;i<nums[n].length;i++){
-            dfs(nums[n][i],lt,nums,track);
+            dfs(nums[n][i],lt,nums);
         }
         lt.remove(lt.size()-1);
-        track[n]=false;
     }
 }
