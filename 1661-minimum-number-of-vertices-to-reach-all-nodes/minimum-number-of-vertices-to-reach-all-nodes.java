de6e7f9583fcfb912664b6438398a7b37065d8e0
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-14 00:25:23 +0530

    Time: 7 ms (98.89%) | Memory: 83.1 MB (29.68%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
@@ -2,29 +2,8 @@ class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> nums) {
         boolean[] track = new boolean[n];
         List<Integer> ans = new ArrayList<>();
-        List<List<Integer>> lt = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            lt.add(new ArrayList<>());
-        }
         for(int i=0;i<nums.size();i++){
-            lt.get(nums.get(i).get(0)).add(nums.get(i).get(1));
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            if(!track[i]){
-                q.add(i);
-                while(!q.isEmpty()){
-                    int val = q.poll();
-                    for(int j=0;j<lt.get(val).size();j++){
-                        int num = lt.get(val).get(j);
-                        if(!track[num]){
-                            track[num]=true;
-                            q.add(num);
-                        }
-                    }
-
-                }
-            }
+            track[nums.get(i).get(1)]=true;
         }
         for(int i=0;i<n;i++) if(!track[i])ans.add(i);
         return ans;
