a5e1cecd09b9b63512b38917042a183f03e16f9f
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-19 11:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (5.61%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,9 +1,5 @@
 class Solution {
-    public static Map<Integer,String> map = new HashMap<>();
-    static{
-        map.put(2,"abc"); map.put(3,"def"); map.put(4,"ghi"); map.put(5,"jkl");
-        map.put(6,"mno"); map.put(7,"pqrs"); map.put(8,"tuv"); map.put(9,"wxyz");
-    }
+    public static String[]  st = {"0","0","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public static List<List<Character>> lt;
     public static List<String> ans;
     public List<String> letterCombinations(String str) {
@@ -11,7 +7,7 @@ class Solution {
         for(int i=0;i<str.length();i++) lt.add(new ArrayList<>());
         for(int i=0;i<str.length();i++){
             int val = str.charAt(i)-'0';
-            String s = map.get(val);
+            String s = st[val];
             for(int j=0;j<s.length();j++){
                 lt.get(i).add(s.charAt(j));
             }
