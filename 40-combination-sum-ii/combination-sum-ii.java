a6085d884b1253068434b2db54ae05d1667ce177
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-10-20 11:42:06 +0530

    Time: 4 ms (37.90%) | Memory: 43.1 MB (87.24%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,34 +1,25 @@
 class Solution {
     public static List<List<Integer>> ans;
-    // public static Set<String> set;
+    public static List<Integer> lt;
     public List<List<Integer>> combinationSum2(int[] nums, int k) {
         Arrays.sort(nums);
         this.ans = new ArrayList<>();
-        // this.set = new HashSet<>();
-        dfs(0,nums,k,new ArrayList<>());
+        this.lt = new ArrayList<>();
+        dfs(0,nums,k);
         return ans;
     }
-    public static void dfs(int idx, int[] nums, int k, List<Integer> lt){
+    public static void dfs(int idx, int[] nums, int k){
         if(k==0){
-            // Collections.sort(lt);
-            // StringBuilder sb=  new StringBuilder();
-            // for(int i:lt)sb.append(i);
-            // String str = sb.toString();
-            // System.out.println(str);
-            // if(!set.contains(str)){
-                ans.add(new ArrayList<>(lt));
-            //     set.add(str);
-            // }
+            ans.add(new ArrayList<>(lt));
             return;
         }
         if(k<0 || idx==nums.length) return;
         lt.add(nums[idx]);
-        dfs(idx+1,nums,k-nums[idx],lt);
+        dfs(idx+1,nums,k-nums[idx]);
         while(idx+1<nums.length && nums[idx]==nums[idx+1]){
             idx++;
         }
         lt.removeLast();
-        dfs(idx+1,nums,k,lt);
+        dfs(idx+1,nums,k);
     }
 }
-// 1 2 2 2 5 
