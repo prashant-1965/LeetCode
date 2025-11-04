8e149bab60eeaafa7a20dd710a5625bd6ab1a402
Prashant Bhardwaj <122390182+prashant-1965@users.noreply.github.com>
2025-11-04 11:00:30 +0530

    Time: 24 ms (82.06%) | Memory: 44.4 MB (23.92%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,13 +1,28 @@
 class Solution {
-    public Integer[][] dp;
     public int numSquares(int n) {
-        this.dp = new Integer[(int)Math.floor(Math.sqrt(n))+1][n+1];
-        return dfs(1,n);
-    }
-    public int dfs(int i, int n){
-        if(n==0) return 0;
-        if(n-i*i<0) return 10001;
-        if(dp[i][n]!=null) return dp[i][n];
-       return dp[i][n] = Math.min(1+dfs(i,n-i*i),dfs(i+1,n));
+        int[] dp = new int[n+1];
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int min = 10001;
+            for(int j=1;j*j<=i;j++){
+                min = Math.min(min,1+dp[i-j*j]);
+            }
+            dp[i]=min;
+        }
+        return dp[n];
     }
 }
+
+// class Solution {
+//     public Integer[][] dp;
+//     public int numSquares(int n) {
+//         this.dp = new Integer[(int)Math.floor(Math.sqrt(n))+1][n+1];
+//         return dfs(1,n);
+//     }
+//     public int dfs(int i, int n){
+//         if(n==0) return 0;
+//         if(n-i*i<0) return 10001;
+//         if(dp[i][n]!=null) return dp[i][n];
+//        return dp[i][n] = Math.min(1+dfs(i,n-i*i),dfs(i+1,n));
+//     }
+// }
